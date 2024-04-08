413822dd34781cde03a2f63983327c2167747c28
Soryeil <88127185+Cloudyyds@users.noreply.github.com>
2024-04-08 20:54:24 +0800

    Update NewsUtil.java

--- TgBot/src/main/java/Dash/NewsUtil.java
+++ TgBot/src/main/java/Dash/NewsUtil.java
@@ -8,7 +8,7 @@ import com.alibaba.fastjson.JSONObject;
 public class NewsUtil{
 
     private static final String URL = "http://v.juhe.cn/toutiao/index";
-    private static final String KEY = "57a1d2f7805864c10bd8336838125dc7";
+    private static final String KEY = "apikey";
 
     public static String getNews() throws Exception {
 
