b62e1fba7563c619c7e3205ac81f35147cc56d22
Soryeil <88127185+Cloudyyds@users.noreply.github.com>
2024-04-08 20:55:56 +0800

    Update config.py

--- qqbot/config.py
+++ qqbot/config.py
@@ -2,6 +2,6 @@ import re
 from nonebot.default_config import *
 
 COMMAND_START = ['', re.compile(r'[/!]+')]
-SUPERUSERS = {1920623062}
+SUPERUSERS = {the master's QQ of bot}
 HOST = '127.0.0.1'
 PORT = 8088
